dd23cbcb351668b00d8478f6992a8679746871c5
theburningmonk <theburningmonk@gmail.com>
2017-08-26 02:21:20 +0100

    - avoid subscribing the destination function's own log group to itself

--- functions/subscribe/handler.js
+++ functions/subscribe/handler.js
@@ -4,11 +4,12 @@ const co             = require('co');
 const Promise        = require('bluebird');
 const AWS            = require('aws-sdk');
 const cloudWatchLogs = Promise.promisifyAll(new AWS.CloudWatchLogs());
-const destinationArn = process.env.DEST_FUNC;
+const destArn        = process.env.DEST_FUNC;
+const destFuncName   = destArn.split(":").reverse()[0];
 
 let subscribe = function* (logGroupName) {
   let options = {
-    destinationArn : destinationArn,
+    destinationArn : destArn,
     logGroupName   : logGroupName,
     filterName     : 'ship-logs',
     filterPattern  : ''
@@ -20,11 +21,17 @@ let subscribe = function* (logGroupName) {
 module.exports.handler = co.wrap(function* (event, context, callback) {
   console.log(JSON.stringify(event));
   
+  // eg. /aws/lambda/logging-demo-dev-api
   let logGroupName = event.detail.requestParameters.logGroupName;
   console.log(`log group: ${logGroupName}`);
 
-  yield subscribe(logGroupName);
-  console.log(`subscribed [${logGroupName}] to [${destinationArn}]`);
-
-  callback(null, 'ok');
+  if (logGroupName === `/aws/lambda/${destFuncName}`) {
+    console.log("ignoring the log group for the ship-logs function to avoid invocation loop!");
+    callback(null, 'ignored');
+  } else {
+    yield subscribe(logGroupName);
+    console.log(`subscribed [${logGroupName}] to [${destArn}]`);
+  
+    callback(null, 'ok');
+  }
 });
