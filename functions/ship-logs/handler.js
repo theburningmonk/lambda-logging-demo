5262b035fde12266993d43f1d2e961ab24d82649
theburningmonk <theburningmonk@gmail.com>
2017-08-26 23:32:37 +0100

    - fixed payload shape

--- functions/ship-logs/handler.js
+++ functions/ship-logs/handler.js
@@ -1,9 +1,19 @@
 'use strict';
 
-const co = require('co');
+const co         = require('co');
+const Promise    = require('bluebird');
 const processAll = require('./lib');
+const zlib       = Promise.promisifyAll(require('zlib'));
 
 module.exports.handler = co.wrap(function* (event, context, callback) {
-  yield processAll(event.logGroup, event.logStream, event.logEvents);
-  callback(null, `Successfully processed ${event.logEvents.length} log events.`);
+  console.log(JSON.stringify(event));
+  
+  let payload = new Buffer(event.awslogs.data, 'base64');
+  let json = (yield zlib.gunzipAsync(payload)).toString('utf8');
+  console.log(json);
+
+  let logEvent = JSON.parse(json);
+
+  yield processAll(logEvent.logGroup, logEvent.logStream, logEvent.logEvents);
+  callback(null, `Successfully processed ${logEvent.logEvents.length} log events.`);
 });
